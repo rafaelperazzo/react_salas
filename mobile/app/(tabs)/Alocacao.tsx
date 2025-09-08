9b0162f4b328154ff916b017026f957c43f6da54
Rafael Perazzo <rafaelperazzo@gmail.com>
2025-09-08 15:32:28 -0300

    feat: Incluído pasta do projeto mobilew

---
+++ mobile/app/(tabs)/Alocacao.tsx
@@ -0,0 +1,37 @@
+import { Image } from 'expo-image';
+import { StyleSheet} from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+
+export default function Alocacao() {
+    return (
+        <ParallaxScrollView
+            headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+            headerImage={
+                    <Image
+                      source={require('@/assets/images/dc_logo.png')}
+                      style={styles.reactLogo}
+                    />
+                  }>
+            <ThemedView style={styles.container}>
+                <ThemedText>Alocação de Salas</ThemedText>
+            </ThemedView>
+        </ParallaxScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    reactLogo: {
+    height: '80%',
+    width: '100%',
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
